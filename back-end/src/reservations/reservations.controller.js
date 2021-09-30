49f76c2c565df5960e3aa6418126044c1db753f1
Linh <lcahu91@gmail.com>
2021-09-30 10:05:31 -0400

    replaced today function with utils function

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,26 +1,17 @@
 /**
  * List handler for reservation resources
  */
+import { today } from "../../../front-end/src/utils/date-time";
+
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-function _getCurrentDate() {
-  let today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth() + 1; // January is 0;
-  let yyyy = today.getFullYear();
-
-  if (dd < 10) dd = "0" + dd;
-  if (mm < 10) mm = "0" + mm;
-
-  return `${yyyy}-${mm}-${dd}`;
-}
 
 async function list(req, res) {
   let { date } = req.query;
 
   if (!date) {
-    date = _getCurrentDate();
+    date = today();
   }
   const data = await service.list(date);
   res.json({ data });
