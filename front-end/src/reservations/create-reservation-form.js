23f7300a629b782b01aadd4a3a10477f96695bdc
Linh <lcahu91@gmail.com>
2021-09-30 10:22:11 -0400

    added reservations/new route

---
+++ front-end/src/reservations/create-reservation-form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+    return (
+      <div className="NotFound">
+        <h1>Form Here</h1>
+      </div>
+    );
+  }
+  
+  export default NotFound;
