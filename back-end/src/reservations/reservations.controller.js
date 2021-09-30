da258347213c192d7123be8ca606efb9e47e668d
Linh <lcahu91@gmail.com>
2021-09-30 10:24:37 -0400

    not sure how to make default today for date

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,7 +1,9 @@
 /**
  * List handler for reservation resources
  */
-import { today } from "../../../front-end/src/utils/date-time";
+
+// TODO: need to figure out how to make today's date as default for dashboard
+// import { today } from "../../../front-end/src/utils/date-time";
 
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
@@ -10,9 +12,9 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 async function list(req, res) {
   let { date } = req.query;
 
-  if (!date) {
-    date = today();
-  }
+  // if (!date) {
+  //   date = today();
+  // }
   const data = await service.list(date);
   res.json({ data });
 }
