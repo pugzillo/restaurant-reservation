a7c6cd97822876777978281e1aba8c8b1f6fab13
Linh <lcahu91@gmail.com>
2021-10-03 09:49:06 -0400

    added reservations table and button visuals

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import formatReservationTime from "../utils/format-reservation-time";
 
 /**
  * Defines the dashboard page.
@@ -26,11 +27,44 @@ function Dashboard({ date }) {
   return (
     <main>
       <h1>Dashboard</h1>
+      <ErrorAlert error={reservationsError} />
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
       </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <div className="DateControlButtons">
+        <button type="button" class="btn btn-secondary">
+          Previous
+        </button>
+        <button type="button" class="btn btn-primary">
+          Today
+        </button>
+        <button type="button" class="btn btn-secondary">
+          Next
+        </button>
+      </div>
+
+      <table class="table table-striped">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Mobile Number</th>
+            <th scope="col">Reservation Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservations.map((reservation) => {
+            return (
+              <tr>
+                <td>{reservation.first_name}</td>
+                <td>{reservation.last_name}</td>
+                <td>{reservation.mobile_number}</td>
+                <td>{reservation.reservation_time}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 }
