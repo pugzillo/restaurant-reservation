d0f31b569fbdd5aac965281abc75ea2520ef45c6
Linh <lcahu91@gmail.com>
2021-10-04 17:33:23 -0400

    added form html

--- front-end/src/tables/new-table-form.js
+++ front-end/src/tables/new-table-form.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function NewTableForm() {
-    return "Hi"
+  // tracks form state
+  const [form, setForm] = useState({
+    table_name: "",
+    capacity: 0,
+  });
+
+  // Changes form when submitted
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitted")
+  };
+
+  const history = useHistory(); 
+
+  const handleCancel = (event) => {
+    history.push("/dashboard"); // send user to home after canceling
+  };
+
+  return (
+    <div className="NewTableForm">
+      <h1>Create New Table</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="TableName">
+          <label>
+            Table Name:
+            <input type="text" name="table_name" onChange={changeHandler} />
+          </label>
+        </div>
+        <div className="Capacity">
+          <label>
+            Capacity:
+            <input type="number" name="capacity" onChange={changeHandler} />
+          </label>
+        </div>
+        <div className="FormButtons">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+          <button className="btn btn-secondary" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default NewTableForm;
