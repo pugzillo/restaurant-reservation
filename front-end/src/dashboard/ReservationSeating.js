a919ddd048e38fc26ad1e0e710513c329b7af940
Linh <lcahu91@gmail.com>
2021-10-07 16:39:10 -0400

    added error messaging

--- front-end/src/dashboard/ReservationSeating.js
+++ front-end/src/dashboard/ReservationSeating.js
@@ -3,8 +3,6 @@ import { useParams, useHistory } from "react-router-dom";
 import { listTables, getReservation, seatReservation } from "../utils/api";
 import { Message } from "semantic-ui-react";
 
-// TODO: Deal with error messaging 
-
 /**
  * Seat reservations at tables
  */
@@ -17,7 +15,6 @@ function ReservationSeating() {
 
   useEffect(loadReservation, [reservation_id]);
   useEffect(loadDropDown, [reservation_id]);
-
 
   // loads reservation information
   function loadReservation() {
@@ -41,8 +38,10 @@ function ReservationSeating() {
 
   // Changes form when selected
   const changeHandler = (event) => {
-    setSelectedTable(event.target.value);
-    console.log(selectedTable);
+    const currentTable = tables.filter(
+      (table) => table.table_id === parseInt(event.target.value)
+    )[0];
+    setSelectedTable(currentTable);
   };
 
   const history = useHistory();
@@ -51,11 +50,16 @@ function ReservationSeating() {
   const submitHandler = (event) => {
     event.preventDefault();
     if (selectedTable.capacity >= reservation.people) {
-      seatReservation(reservation_id, selectedTable);
+      seatReservation(reservation_id, selectedTable.table_id);
       history.push("/dashboard"); // send user to home after canceling
     } else {
-      const err = new Error("Selected table does not have the capacity for this reservation.");
-
+      // const err = new Error(
+      //   "Selected table does not have the capacity for this reservation."
+      // );
+      setFormErrors([
+        ...formErrors,
+        "Selected table does not have the capacity for this reservation.",
+      ]);
     }
   };
 
@@ -66,6 +70,17 @@ function ReservationSeating() {
   return (
     <div className="ReservationSeating">
       <h1>Seat a Reservation</h1>
+      {formErrors.length !== 0 && (
+        <Message negative className="alert alert-danger">
+          <Message.Header>Warning:</Message.Header>
+          <ul>
+            {formErrors.map((err) => {
+              return <li>{err}</li>;
+            })}
+          </ul>
+        </Message>
+      )}
+      {console.log(formErrors)}
       <form onSubmit={submitHandler}>
         <div className="form-group row">
           <div className="col-auto my-1">
