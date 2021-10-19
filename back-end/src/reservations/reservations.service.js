e8d5ee2bbde1c3f84b99e6f8781a4d1550360e03
Linh <lcahu91@gmail.com>
2021-10-19 15:39:08 -0400

    add search query for mobile number

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -26,9 +26,19 @@ function update(updatedReservation) {
     .then((updatedRecords) => updatedRecords[0]);
 }
 
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+    "translate(mobile_number, '() -', '') like ?",
+    %${mobile_number.replace(/\D/g, "")}%
+    )
+    .orderBy("reservation_date");
+}
+
 module.exports = {
   list,
   create,
   read,
   update,
+  search,
 };
