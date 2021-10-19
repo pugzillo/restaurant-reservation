7889130bf15bf6de527b1a0a8fc8412781ab06e2
Linh <lcahu91@gmail.com>
2021-10-19 15:39:48 -0400

    formating

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -29,11 +29,12 @@ function update(updatedReservation) {
 function search(mobile_number) {
   return knex("reservations")
     .whereRaw(
-    "translate(mobile_number, '() -', '') like ?",
-    %${mobile_number.replace(/\D/g, "")}%
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
     )
     .orderBy("reservation_date");
 }
+
 
 module.exports = {
   list,
