d5c853a065eff4f1081577bc5935045f8ba65c0a
Linh <lcahu91@gmail.com>
2021-10-19 14:56:31 -0400

    added search form

--- front-end/src/search/search.js
+++ front-end/src/search/search.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Search() {
-  return "hi";
+  return (
+    <div className="search">
+      <h1>Search</h1>
+
+      <form>
+        <div class="form-group">
+          <input
+            type="text"
+            name="mobile_number"
+            placeholder="Enter a customer's phone number"
+            class="form-control"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Find
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Search;
